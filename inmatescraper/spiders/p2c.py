dfcc63b0749079b2e76682b1145eca01ae9a5d2b
wkwilson <wkwilson@lockss.org>
2020-07-08 00:57:06 -0400

    p2c update

--- inmatescraper/spiders/p2c.py
+++ inmatescraper/spiders/p2c.py
@@ -7,12 +7,27 @@ import json
 class P2CSpider(scrapy.Spider):
     name = 'P2C'
     QUERY_URL_SUFFIX = '/jqHandler.ashx?op=s'
-    P2C_DEFAULT_FORMDATA = {'t': 'ii', '_search': 'false', 'rows': '100000', 'page': '1', 'sidx': 'disp_name',
-                            'sord': 'asc'}
-    HEADER_STRING = 'Date Charged'
-    P2C_DEFAULT_HEADER = ''
+    DEFAULT_FORMDATA = {'t': 'ii', '_search': 'false', 'rows': '100000', 'page': '1', 'sidx': 'disp_name',
+                        'sord': 'asc'}
 
-    def __init__(self, **kwargs):
+    DEFAULT_INMATE_MAP = {items.KEY_FIRST_NAME: 'firstname',
+                          items.KEY_MIDDLE_NAME: 'middlename',
+                          items.KEY_LAST_NAME: 'lastname',
+                          items.KEY_GENDER: 'sex',
+                          items.KEY_DATE_OF_BIRTH: 'dob',
+                          items.KEY_RACE: 'race',
+                          items.KEY_BOOK_ID: 'book_id',}
+
+    DEFAULT_CHARGE_MAP = {items.KEY_DATE_CONFINED: 'date_arr',
+                          items.KEY_DESC: 'disp_charge',
+                          items.KEY_AGENCY: 'disp_agency',}
+    inmate_map = {}
+    charge_map = {}
+
+    def __init__(self, start_urls, inmate_map=DEFAULT_INMATE_MAP, charge_map=DEFAULT_CHARGE_MAP, **kwargs):
+        self.start_urls = start_urls
+        self.inmate_map = inmate_map
+        self.charge_map = charge_map
         ##TODO take input state and county
         super().__init__(**kwargs)
 
@@ -20,39 +35,26 @@ class P2CSpider(scrapy.Spider):
         for url in self.start_urls:
             post_url = url + self.QUERY_URL_SUFFIX
             ##TODO figure out required headers
-            post_headers = {'POST': url + self.QUERY_URL_SUFFIX + ' HTTP/1.1',
-                            'Connection': 'keep-alive',
-                            'Accept': 'application/json, text/javascript, */*; q=0.01',
-                            'Origin': url,
-                            'X-Requested-With': 'XMLHttpRequest',
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                            'Accept-Encoding': 'gzip, deflate',
-                            'Accept-Language': 'en-US,en;q=0.9',
-                            'Cache-Control': 'no-cache'}
             yield scrapy.FormRequest(dont_filter=True,
-                                      formdata=self.P2C_DEFAULT_FORMDATA,
-                                      headers=post_headers,
-                                      url=post_url,)
+                                     formdata=self.P2C_DEFAULT_FORMDATA,
+                                     url=post_url,)
 
-    ##TODO map data names as a spider argument
     def parse(self, response):
         jsonresponse = json.loads(response.text)
         inmates = []
         for inmate_data in jsonresponse['rows']:
             inmate_loader = ItemLoader(items.Inmate())
-            inmate_loader.add_value(items.KEY_FIRST_NAME, inmate_data['firstname'])
-            inmate_loader.add_value(items.KEY_MIDDLE_NAME, inmate_data['middlename'])
-            inmate_loader.add_value(items.KEY_LAST_NAME, inmate_data['lastname'])
-            inmate_loader.add_value(items.KEY_GENDER, inmate_data['sex'])
-            inmate_loader.add_value(items.KEY_DATE_OF_BIRTH, inmate_data['dob'])
-            inmate_loader.add_value(items.KEY_RACE, inmate_data['race'])
-            inmate_loader.add_value(items.KEY_BOOK_ID, inmate_data['book_id'])
+
+            for key, value in self.inmate_map:
+                inmate_loader.add_value(key, inmate_data[value])
+
             inmate = inmate_loader.load_item()
             inmate[items.KEY_CHARGES] = []
             charge_loader = ItemLoader(items.Charge())
-            charge_loader.add_value(items.KEY_DATE_CONFINED, inmate_data['date_arr'])
-            charge_loader.add_value(items.KEY_DESC, inmate_data['disp_charge'])
-            charge_loader.add_value(items.KEY_AGENCY, inmate_data['disp_agency'])
+
+            for key, value in self.charge_map:
+                charge_loader.add_value(key, inmate_data[value])
+
             inmate[items.KEY_CHARGES].append(charge_loader.load_item())
             inmates.append(inmate)
         return inmates
